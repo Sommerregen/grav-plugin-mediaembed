1d2c8a2c137de56e9fd1bb2ab3c9b8d91cb672e9
Sommerregen <sommerregen@users.noreply.github.com>
2015-05-17 13:58:15 +0200

    Added support for **Slides.com SlideDesk** as requested in issue #4

---
+++ classes/Services/Slides.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Slides.com
+ *
+ * Present your ideas - Slides is a place for creating, presenting and
+ * sharing presentations.
+ *
+ * Licensed under MIT, see LICENSE.
+ */
+
+namespace Grav\Plugin\MediaEmbed\Services;
+
+use Grav\Plugin\MediaEmbed\OEmbed\OEmbedRich;
+
+/**
+ * Slides
+ */
+class Slides extends OEmbedRich
+{
+	public function getOEmbed()
+  {
+    if ($this->oembed) {
+      return $this->oembed;
+    }
+
+    $endpoint = $this->format($this->config->get('endpoint', ''));
+    if (!$endpoint) {
+      return [];
+    }
+
+    // Extract owner from embed code
+    list($owner, $id) = explode('/', $this->embedCode, 2);
+
+    // Fake response
+    $this->oembed = [
+    	'type' => 'rich',
+    	'title' => '',
+    	'description' => '',
+			'author_name' => $owner,
+			'author_url' => 'http://slides.com/'.$owner,
+			'provider' => 'Slides',
+			'provider_url' => 'http://slides.com',
+			'url' => 'http://slides.com/'.$this->embedCode,
+      'html' => '<iframe src="//slides.com/'.rtrim($this->embedCode, '/').'/embed" width="576" height="420" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
+      'width' => 576,
+      'height' => 420,
+		];
+
+    return $this->oembed;
+  }
+
+  public function getEmbedCode($params = [])
+  {
+    $embed = parent::getEmbedCode($params);
+    if ($this->embedCode && $this->oembed) {
+      // Inject parameters directly into HTML OEmbed attribute
+      $query = http_build_query($this->params());
+      $url = $this->attributes['protocol'].'slides.com/'.rtrim($this->embedCode, '/').'/embed';
+      if (mb_strlen($query) > 0) {
+        $url .= (false === strpos($url, '?') ? '?' : '&') . $query;
+      }
+
+      // Get width and height
+      $width = $this->attributes['width'];
+      $height = $this->attributes['height'];
+
+      $embed = '<iframe src="'.$url.'" width="'.$width.'" height="'.$height.'" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+    }
+
+    return $embed;
+  }
+}
